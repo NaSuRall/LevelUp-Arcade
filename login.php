58d48e67e1a01bdc80296a705bbe755559f17be8
NaSuRal <romain.poulain@edu.esiee-it.fr>
2024-11-17 22:06:35 +0100

    Add Css page Login

--- login.php
+++ login.php
@@ -6,18 +6,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/login.css">
     <title>Login</title>
 </head>
 <body>
     <div class="container">
-        <form action="traitement.php" method="POST">
-            <label for="email">Email :</label>
-            <input type="text" id="email" name="email" placeholder="Email">
-            <label for="mdp">Mot de pase :</label>
-            <input type="password" name="mdp" id="mdp">
-            <input type="submit" id="envoie" name="envoie">
-        </form>
-        <a href="./inscription.php">Inscription</a>
+        <div class="formulaire">
+            <h1>Sing in</h1>
+            <div class="border"></div>
+            <form action="traitement.php" method="POST">
+                <input type="text" id="email" name="email" placeholder="Email">
+                <input type="password" name="mdp" id="mdp" placeholder="Password">
+                <span><input type="submit" id="envoie" name="envoie" value="Se Connecter"></span>
+            </form>
+            <a href="./inscription.php">Inscription</a>
+        </div>
     </div>
 </body>
 </html>
